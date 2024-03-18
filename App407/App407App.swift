fd214ed20f3de428260b7dc8ac3794586d7f5617
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-03-18 10:36:51 +0300

    Initial Commit

---
+++ App407/App407App.swift
@@ -0,0 +1,17 @@
+//
+//  App407App.swift
+//  App407
+//
+//  Created by IGOR on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct App407App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
